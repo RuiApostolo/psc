25ced198d926fcf028fcc8f9b88c505e2b152125
Rui Apóstolo <ruiapostolo@gmail.com>
2020-03-03 19:17:48 +0000

    outputs number of side-chains

--- psc.py
+++ psc.py
@@ -17,8 +17,37 @@ alphabet = [
     '*', '-'
 ]
 
-c = Counter()
+values = {'A': 1,
+          'B': 1,
+          'C': 1,
+          'D': 2,
+          'E': 2,
+          'F': 1,
+          'G': 1,
+          'H': 1,
+          'I': 1,
+          'J': 1,
+          'K': 1,
+          'L': 1,
+          'M': 1,
+          'N': 1,
+          'O': 1,
+          'P': 0,
+          'Q': 1,
+          'R': 1,
+          'S': 1,
+          'T': 1,
+          'U': 1,
+          'V': 1,
+          'W': 1,
+          'X': 0,
+          'Y': 1,
+          'Z': 1,
+          '*': 0,
+          '-': 0
+          }
 
+c = Counter()
 
 f = open("P69905.fasta", 'r')
 lines = iter(f)
@@ -34,6 +63,7 @@ c['\n'] = 0
 print("Frequency list:")
 for i in alphabet:
     print(i, c[i])
+print('')
 
 c += Counter()
 
@@ -42,5 +72,15 @@ for i, j in c.items():
     if i not in alphabet:
         if b is True:
             print("Wrong symbols list:")
+            print('')
             b = False
         print(i, c[i])
+
+t = 0
+for i, j in c.items():
+    if i is 'X':
+        print('Special case, X can be anythin. Counts of X = ', c[i])
+    t += c[i] * values[i]
+
+print('Total number of possible side-chains is: ', t)
+print('')
