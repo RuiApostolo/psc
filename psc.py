a93d6fa3007693822832ee5a5d510aa3b698f51c
Rui Apóstolo <ruiapostolo@gmail.com>
2020-03-04 13:09:56 +0000

    added comments, improved input handling, added support for choosing input file name

--- psc.py
+++ psc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from collections import Counter
+import sys,argparse
 
 # Proline       P = 0
 # Aspartic Acid D = 2
@@ -7,16 +8,19 @@ from collections import Counter
 # B and Z ?
 # U ?
 # X special case - counted separately
-# J, O doens't exist
+# J, O doesn't exist
 # * translation stop
 # - gap
 # All others      = 1
+
+# List of accepted characters
 alphabet = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
     '*', '-'
 ]
 
+# Values attributed to each character
 values = {'A': 1,
           'B': 1,
           'C': 1,
@@ -47,26 +51,34 @@ values = {'A': 1,
           '-': 0
           }
 
+# Define arguments from command line, and generate help text
+parser = argparse.ArgumentParser(description='Calculate the number of side chains in a protein from a fasta file.')
+parser.add_argument('inputfile', metavar='fasta_file', type=str, help='Path to the input fasta file.')
+# parser.add_argument('--output', '-o', help='Name for output file.', default='Nsidechains.txt')
+# Parse arguments
+args = parser.parse_args()
+# Create empty counter
 c = Counter()
-
-f = open("P69905.fasta", 'r')
+# open file
+f = open(args.inputfile, 'r')
+# Separate file into lines
 lines = iter(f)
-# Skip first line. - change to check first character
+# Skip first line
 next(f)
+# Iterate through lines and add to counter
 for line in lines:
     c.update(line)
-
 # Remove carriage return symbol
 c['\n'] = 0
-# print(list(c.elements()))
+# Delete empty key-item pairs (carriage return)
+c += Counter()
 # Print frequency list
 print("Frequency list:")
 for i in alphabet:
     print(i, c[i])
 print('')
 
-c += Counter()
-
+# Check for items not in accepted list and print them
 b = True
 for i, j in c.items():
     if i not in alphabet:
@@ -76,10 +88,17 @@ for i, j in c.items():
             b = False
         print(i, c[i])
 
+# Extra empy line if wrong symbols are present
+if b is False:
+    print('')
+
+# Sum amount of possible side-chains
 t = 0
 for i, j in c.items():
     if i is 'X':
-        print('Special case, X can be anythin. Counts of X = ', c[i])
+        # Print special cases (X - any AA)
+        print('Special case, X can be anything. Counts of X = ', c[i])
+        print('')
     t += c[i] * values[i]
 
 print('Total number of possible side-chains is: ', t)
